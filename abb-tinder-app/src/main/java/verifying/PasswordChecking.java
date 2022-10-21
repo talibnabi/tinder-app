7e7ad6d063e427e484bf104a29b601ab504d0a7b
talibnabi <talibnabiyev@gmail.com>
2022-10-21 09:18:19 -0700

    commit

--- abb-tinder-app/src/main/java/verifying/PasswordChecking.java
+++ abb-tinder-app/src/main/java/verifying/PasswordChecking.java
@@ -6,7 +6,7 @@ import jakarta.servlet.ServletRequest;
 public class PasswordChecking {
     public static boolean checkPasswordEquals(ServletRequest req) {
         String password = req.getParameter("password");
-        String repassword = req.getParameter("repassword");
+        String repassword = req.getParameter("passwordrepeat");
 
         if (password == null && repassword == null) return true;
         assert password != null;
