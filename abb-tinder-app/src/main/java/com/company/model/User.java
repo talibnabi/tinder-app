fa711e13e93e481ab18a2ccebe3e044c7a9cb078
talibnabi <talibnabiyev@gmail.com>
2022-10-29 05:18:35 -0700

    commit

--- abb-tinder-app/src/main/java/com/company/model/User.java
+++ abb-tinder-app/src/main/java/com/company/model/User.java
@@ -1,12 +1,14 @@
 package com.company.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.Objects;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class User {
 
     private final String name;
@@ -15,19 +17,6 @@ public class User {
     private final String picURL;
     private final int age;
     private final String password;
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User user)) return false;
-        return getAge() == user.getAge() && getName().equals(user.getName()) && getSurname().equals(user.getSurname()) && getEmail().equals(user.getEmail()) && getPicURL().equals(user.getPicURL()) && getPassword().equals(user.getPassword());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getName(), getSurname(), getEmail(), getPicURL(), getAge(), getPassword());
-    }
 
     @Override
     public String toString() {
